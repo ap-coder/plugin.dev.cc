3741384bae528d5ef8e804adf85f1ce057accf8e
ap-coder <apcoder2019@gmail.com>
2020-04-09 12:12:20 -0600

    renamed directory
    changed from software to code-software

--- wp-content/plugins/software/framework/include/routes.php
+++
@@ -1,4 +0,0 @@
-<?php
-$framework_routes = new \Framework\Route;
-$framework_routes->ajax('path_to_index_method_of_indexController');
-$framework_routes->ajax('path_to_login_of_indexController', 'index/login');
--- wp-content/plugins/software/framework/source/v1/views/account/partial/headshot.php
+++
@@ -1,5 +0,0 @@
-<?php if( !empty($data['profile_info']['headshot']) ) : ?>
-<img src="<?php echo image_url($data['profile_info']['headshot']); ?>" alt="">
-<?php else : ?>
-<img src="https://placehold.it/490x490" alt="">
-<?php endif;?>
